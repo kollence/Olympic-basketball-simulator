9beafeb059654cc39ed28bd08fc5aadfae745dea
kole <misterija@gmail.com>
2024-09-03 22:33:13 +0200

    Going trough steps to the finale

--- src/components/BasketballTournament.jsx
+++ src/components/BasketballTournament.jsx
@@ -1,13 +1,47 @@
+import { useState } from "react";
 import { useTournamentData } from "../state/useTournamentData";
 
 export default function BasketballTournament() {
   const { groups, loading } = useTournamentData();
+  const [currentStep, setCurrentStep] = useState(0);
 
-  console.log(loading,groups );
-  
+  console.log(loading, groups);
+
+  const steps = [
+    { component: <div>GroupStage</div> },
+    { component: <div>DrawStage</div> },
+    { component: <div>EliminationPhase</div> },
+  ];
+  const handleNextStep = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
+  const handlePreviousStep = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    }
+  };
+
   return (
     <div>
-      Basketball tournament component
+      <div className="flex justify-between mt-4">
+        <button onClick={handlePreviousStep} hidden={currentStep === 0}>
+          Previous Step
+        </button>
+        <div></div>
+        <button
+          onClick={handleNextStep}
+          hidden={currentStep === steps.length - 1}
+        >
+          Next Step
+        </button>
+      </div>
+      <div className="step-container">
+        {steps.map((step, index) => (
+          <div key={index}>{step.component && step.component}</div>
+        ))}
+      </div>
     </div>
   );
 }
